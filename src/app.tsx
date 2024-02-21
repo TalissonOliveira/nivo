c1bf20ba71eee9cc493fbb44d4c2b840d50df35b
Talisson Oliveira <talissonlux@outlook.com>
2024-02-21 04:42:34 -0300

    feat: implements api data listing using react-query

--- src/app.tsx
+++ src/app.tsx
@@ -5,8 +5,40 @@ import { Tabs } from './components/tabs'
 import { Button } from './components/ui/button'
 import { Control, Input } from './components/ui/input'
 import { Pagination } from './components/pagination'
+import { useQuery } from '@tanstack/react-query'
+
+export interface TagsResponse {
+  first: number
+  prev: number | null
+  next: number
+  last: number
+  pages: number
+  items: number
+  data: Tag[]
+}
+
+export interface Tag {
+  title: string
+  slug: string
+  amountOfVideos: number
+  id: string
+}
 
 export function App() {
+  const { data: tagsResponse, isLoading } = useQuery<TagsResponse>({
+    queryKey: ['get-tags'],
+    queryFn: async () => {
+      const response = await fetch('http://localhost:3333/tags?_page=1&_per_page=10')
+      const data = await response.json()
+      console.log(data)
+      return data
+    },
+  })
+
+  if (isLoading) {
+    return null
+  }
+
   return (
     <div className="py-4 space-y-8">
       <div>
@@ -43,17 +75,17 @@ export function App() {
             <TableHead></TableHead>
           </TableHeader>
           <TableBody>
-            {Array.from({ length: 10 }).map((_, index) => (
-              <TableRow key={index}>
+            {tagsResponse?.data.map((tag) => (
+              <TableRow key={tag.id}>
                 <TableCell></TableCell>
                 <TableCell>
                   <div className="flex flex-col gap-0.5">
-                    <span className="font-medium">React</span>
-                    <span className="text-xs text-zinc-500">01518E42-4D55-4390-A2E7-278C83332EA3</span>
+                    <span className="font-medium">{tag.title}</span>
+                    <span className="text-xs text-zinc-500">{tag.id}</span>
                   </div>
                 </TableCell>
                 <TableCell className="text-zinc-300">
-                  13 video(s)
+                  {tag.amountOfVideos} video(s)
                 </TableCell>
                 <TableCell className="text-right">
                   <Button size="icon">
